d548c36b50675a2b1799f29b6ac45b8036d7e5c0
monishsoni427 <soni2000monish@gmail.com>
2023-07-25 17:47:23 +0530

    last chance

---
+++ frontend/build/precache-manifest.a6e6c7da652a734e1b22520467860475.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f932fc0ca73bcd841d28365d4b690f61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "198e83e55a01000cc12c",
+    "url": "/static/css/main.2556c45e.chunk.css"
+  },
+  {
+    "revision": "002a9e8a3d53208219c5",
+    "url": "/static/js/2.95fb10ae.chunk.js"
+  },
+  {
+    "revision": "198e83e55a01000cc12c",
+    "url": "/static/js/main.495ac4d0.chunk.js"
+  },
+  {
+    "revision": "c365dd7c495663b5e597",
+    "url": "/static/js/runtime-main.92d67ae5.js"
+  }
+]);
