315b1afa359fd2ad3cc5514fb955e66c3505d8d8
Monish <soni2000monish@gmail.com>
2024-07-25 07:52:00 +0530

    connection time

--- frontend/src/places/pages/UserPlaces.js
+++ frontend/src/places/pages/UserPlaces.js
@@ -16,7 +16,7 @@ const UserPlaces = () => {
     const fetchPlaces = async () => {
       try {
         const responseData = await sendRequest(
-          `http://localhost:5000/api/places/user/${userId}`,
+          `https://your-places-app-758t.vercel.app/api/places/user/${userId}`,
           "GET"
         );
         setLoadedPlaces(responseData.places);
